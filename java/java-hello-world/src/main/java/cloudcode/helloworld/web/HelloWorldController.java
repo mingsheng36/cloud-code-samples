1e61a93d9a3110f5937051a4d7e19b586f6bdb08
mingsheng36 <mingsheng36@gmail.com>
2019-09-21 14:26:30 +0800

    java-translation-service working demo

--- java/java-hello-world/src/main/java/cloudcode/helloworld/web/HelloWorldController.java
+++ java/java-hello-world/src/main/java/cloudcode/helloworld/web/HelloWorldController.java
@@ -1,20 +1,62 @@
 package cloudcode.helloworld.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * defines the REST endpoints managed by the server.
- */
-@RestController
-public final class HelloWorldController {
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloWorldController {
+    private Translate translate;
 
     /**
-     * endpoint for the landing page
-     * @return a simple hello world message
+     * Takes in a json encoded string array to translate
+     * returns an JSON array of the translated text.
      */
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello World!";
+    @RequestMapping(value = "/", produces = "application/json")
+    @ResponseBody
+    public List<Translation> translate(@RequestParam String text) throws JSONException {
+        // Instantiates a client
+        translate = TranslateOptions.getDefaultInstance().getService();
+
+        JSONArray jsonArray = new JSONArray(text);
+
+        List<Translation> translations = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            translations.add(getTranslation(jsonArray.get(i).toString()));
+        }
+
+        return translations;
+    }
+
+    private Translation getTranslation(String text) {
+        String englishTranslation, germanTranslation;
+
+        String incomingLanguage = translate.detect(text).getLanguage();
+        if (!incomingLanguage.equals("en")) {
+            englishTranslation = translate.translate(
+                    text,
+                    Translate.TranslateOption.targetLanguage("en")).getTranslatedText();
+        } else {
+            englishTranslation = text;
+        }
+
+        germanTranslation =
+                translate.translate(
+                        text,
+                        Translate.TranslateOption.targetLanguage("de")).getTranslatedText();
+
+        System.out.printf("Source Language: %s%n", incomingLanguage);
+        System.out.printf("English Translation: %s%n", englishTranslation);
+        System.out.printf("German Translation: %s%n", germanTranslation);
+
+        return new Translation(englishTranslation, germanTranslation);
     }
 }
